2511fdbbcb6e6ad35ec412e6d49a9078f3993004
Myron Langlais <myron.langlais@diffblue.com>
2019-04-16 17:31:56 +0100

    Add tslint disabled logging utility

---
+++ src/utils/log.ts
@@ -0,0 +1,10 @@
+// Copyright 2019 Diffblue Limited. All Rights Reserved.
+
+// tslint:disable-next-line: no-console
+export const error = console.error;
+
+// tslint:disable-next-line: no-console
+export const log = console.log;
+
+// tslint:disable-next-line: no-console
+export const warn = console.warn;
