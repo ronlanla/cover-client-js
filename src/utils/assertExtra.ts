d0d922e3e1bfbf09a595a4be3360c6a78519ff12
Peter West <peterjwest3@gmail.com>
2019-04-18 14:26:21 +0100

    Add assertExtra utility module

---
+++ src/utils/assertExtra.ts
@@ -0,0 +1,55 @@
+// Copyright 2017-2019 Diffblue Limited. All Rights Reserved.
+
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+
+const assertExtra = {
+  // Asserts that a spy was not called
+  notCalled: (spy: sinon.SinonSpy) => {
+    assert.strictEqual(spy.callCount, 0, `Called ${spy.callCount} times`);
+  },
+
+  // Asserts that a spy was called exactly once
+  calledOnce: (spy: sinon.SinonSpy) => {
+    assert.strictEqual(spy.callCount, 1, `Called ${spy.callCount} times`);
+  },
+
+  // Asserts that a spy was called exactly once with the arguments `args`
+  calledOnceWith: <Type>(spy: sinon.SinonSpy, args: Type[]) => {
+    assertExtra.calledOnce(spy);
+    assert.deepStrictEqual(spy.getCall(0).args, args);
+  },
+
+  // Asserts that a spy was called exactly with the arguments specified in each entry of `calls`
+  calledWith: <Type>(spy: sinon.SinonSpy, calls: Type[][]) => {
+    assert.deepStrictEqual(spy.getCalls().map((call) => call.args), calls);
+  },
+
+  // Asserts that a spy was called starting with the arguments specified in each entry of `calls`
+  calledStartingWith: <Type>(spy: sinon.SinonSpy, calls: Type[][]) => {
+    const actualCalls = spy.getCalls().map((call) => call.args);
+    const startingWithCalls = actualCalls.map((args, i) => calls[i] ? args.slice(0, calls[i].length) : args);
+    assert.deepStrictEqual(startingWithCalls, calls);
+  },
+
+  // Asserts that a spy was called exactly once starting with the arguments `args`
+  calledOnceStartingWith: <Type>(spy: sinon.SinonSpy, args: Type[]) => {
+    assertExtra.calledOnce(spy);
+    assert.deepStrictEqual(spy.getCall(0).args.slice(0, args.length), args);
+  },
+
+  // Asserts a promise rejects with a particular error
+  rejectsWith: async <Resolve>(promise: Promise<Resolve>, expectedError: Error | string) => {
+    let rejected = false;
+    return promise.catch((error: Error | string) => {
+      rejected = true;
+      return error;
+    })
+    .then((error: Error | string) => {
+      assert.strictEqual(rejected, true, 'Promise did not reject');
+      assert.strictEqual(String(error), String(expectedError));
+    });
+  },
+};
+
+export default { ...assert, ...assertExtra };
