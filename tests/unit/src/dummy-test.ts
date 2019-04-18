6d9378fba6c92ab4e6ee04d3cfe27450b1bc25fa
Myron Langlais <myron.langlais@diffblue.com>
2019-04-18 12:28:35 +0100

    Create dummy test

---
+++ tests/unit/src/dummy-test.ts
@@ -0,0 +1,10 @@
+// Copyright 2019 Diffblue Limited. All Rights Reserved.
+
+import * as assert from 'assert';
+
+describe('dummy-test', () => {
+  it('Passes', () => {
+    const aString = 'fooBar';
+    assert.deepStrictEqual(aString, 'fooBar');
+  });
+});
