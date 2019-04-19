b780ae259cd823fa4bd3aeafe2af473ccf26ac67
Peter West <peterjwest3@gmail.com>
2019-04-19 13:58:49 +0100

    Use correct type for glob-gitignore return value

--- src/types/glob-gitignore/index.d.ts
+++ src/types/glob-gitignore/index.d.ts
@@ -1,5 +1,5 @@
 // Copyright 2017-2019 Diffblue Limited. All Rights Reserved.
 
 declare module 'glob-gitignore' {
-  export function glob(patterns: string | string[], options: { [key: string]: any }): PromiseLike<string[]>;
+  export function glob(patterns: string | string[], options: { [key: string]: any }): Promise<string[]>;
 }
