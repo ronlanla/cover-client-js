38edc534fc2a44b49324007bbfbedd379500e22d
Myron Langlais <myron.langlais@diffblue.com>
2019-04-23 12:11:28 +0100

    Update log.ts to include changes from branch

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,10 +1,4 @@
 // Copyright 2019 Diffblue Limited. All Rights Reserved.
 
 // tslint:disable-next-line: no-console
-export const error = console.error;
-
-// tslint:disable-next-line: no-console
-export const log = console.log;
-
-// tslint:disable-next-line: no-console
-export const warn = console.warn;
+export default console;
