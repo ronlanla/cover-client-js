ba02657bc0c9c3ac2059327e05a6bc674ef0b031
Peter West <peterjwest3@gmail.com>
2019-04-29 15:40:15 +0100

    Improve documentation for assertExtra

--- src/utils/assertExtra.ts
+++ src/utils/assertExtra.ts
@@ -4,41 +4,41 @@ import * as assert from 'assert';
 import * as sinon from 'sinon';
 
 const assertExtra = {
-  // Asserts that a spy was not called
+  /** Asserts that a spy was not called */
   notCalled: (spy: sinon.SinonSpy) => {
     assert.strictEqual(spy.callCount, 0, `Called ${spy.callCount} times`);
   },
 
-  // Asserts that a spy was called exactly once
+  /** Asserts that a spy was called exactly once */
   calledOnce: (spy: sinon.SinonSpy) => {
     assert.strictEqual(spy.callCount, 1, `Called ${spy.callCount} times`);
   },
 
-  // Asserts that a spy was called exactly once with the arguments `args`
+  /** Asserts that a spy was called exactly once with the arguments `args` */
   calledOnceWith: <Type>(spy: sinon.SinonSpy, args: Type[]) => {
     assertExtra.calledOnce(spy);
     assert.deepStrictEqual(spy.getCall(0).args, args);
   },
 
-  // Asserts that a spy was called exactly with the arguments specified in each entry of `calls`
+  /** Asserts that a spy was called exactly with the arguments specified in each entry of `calls` */
   calledWith: <Type>(spy: sinon.SinonSpy, calls: Type[][]) => {
     assert.deepStrictEqual(spy.getCalls().map((call) => call.args), calls);
   },
 
-  // Asserts that a spy was called starting with the arguments specified in each entry of `calls`
+  /** Asserts that a spy was called starting with the arguments specified in each entry of `calls` */
   calledStartingWith: <Type>(spy: sinon.SinonSpy, calls: Type[][]) => {
     const actualCalls = spy.getCalls().map((call) => call.args);
     const startingWithCalls = actualCalls.map((args, i) => calls[i] ? args.slice(0, calls[i].length) : args);
     assert.deepStrictEqual(startingWithCalls, calls);
   },
 
-  // Asserts that a spy was called exactly once starting with the arguments `args`
+  /** Asserts that a spy was called exactly once starting with the arguments `args` */
   calledOnceStartingWith: <Type>(spy: sinon.SinonSpy, args: Type[]) => {
     assertExtra.calledOnce(spy);
     assert.deepStrictEqual(spy.getCall(0).args.slice(0, args.length), args);
   },
 
-  // Asserts a promise rejects with a particular error
+  /** Asserts a promise rejects with a particular error */
   rejectsWith: async <Resolve>(promise: Promise<Resolve>, expectedError: Error | string | RegExp) => {
     let rejected = false;
     return promise.catch((error: Error | string) => {
@@ -56,15 +56,21 @@ const assertExtra = {
     });
   },
 
+  /**
+   * Asserts that a stub is not called, expected to be used in combination
+   * with `withArgs` so that any other calls fall through to this.
+   * This should be called before calling the function under test
+   */
   notOtherwiseCalled: (stub: sinon.SinonStub, name: string) => {
     stub.callsFake(async (...args) => {
       throw new Error(`Unexpected call to ${name} with args ${JSON.stringify(args)}`);
     });
   },
 
-  matches: (value: string, matcher: RegExp | string) => {
-    const matches = value.match(matcher);
-    assert.strictEqual(matches && matches.length > 0, true, `Pattern "${matcher}" did not match "${value}"`);
+  /** Checks if a value matches a regex or string pattern */
+  matches: (value: string, pattern: RegExp | string) => {
+    const matches = value.match(pattern);
+    assert.strictEqual(matches && matches.length > 0, true, `Pattern "${pattern}" did not match "${value}"`);
   },
 };
 
