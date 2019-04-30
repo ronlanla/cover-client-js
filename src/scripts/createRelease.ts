d0883849633bc6711f9e2b787a7954edc120881d
James Hannett <james.hannett@diffblue.com>
2019-04-30 13:48:03 +0100

    Added in pull request generation (& lint fixes)

--- src/scripts/createRelease.ts
+++ src/scripts/createRelease.ts
@@ -1,68 +1,81 @@
 // Copyright 2019 Diffblue Limited. All Rights Reserved.
 
+import { exec } from 'child_process';
 import { readFile, writeFile } from 'fs';
-import { promisify } from 'util';
-import * as simplegit from 'simple-git/promise';
 import * as inquirer from 'inquirer';
 import * as semver from 'semver';
-
+import * as simplegit from 'simple-git/promise';
+import { promisify } from 'util';
 import argvParser, { Options } from '../utils/argvParser';
-// import { createChangelog, renderChangelog } from '../utils/changelog';
 import logger from '../utils/log';
+
 
 const dependencies = {
   writeFile: promisify(writeFile),
   readFile: promisify(readFile),
+  git: simplegit(),
+  exec: promisify(exec),
 };
 
 const packageJSONFilename = 'package.json';
 
+/** Assurance that 'release_type' will be a member of what inquirer.js will return */
 type PatchTypeAnswer = {
   release_type: semver.ReleaseType;
 };
 
+/**
+ * Begins the release process
+ *
+ * Ensures the Git repo is up to date, bumps the version and creates a PR.
+ *
+ * @param {string[]} args - Array of command line arguments
+ * @param {Options} options - Collection of arguments
+ */
 export default async function beginRelease(args: string[], options: Options) {
   if (options.help) {
     logger.log(helpMessage());
     return;
   }
 
-  var dryRun:boolean = false;
-  if(options.dry) {
+  let dryRun = false;
+  if (options.dry) {
     dryRun = true;
   }
 
-  const git = simplegit();
-
   // make sure develop and master are up to date
   // also get all tags
-  logger.info("Pulling branches and tags...");
-  await git.fetch(['--tags']);
-  await git.pull('origin', 'master');
-  await git.pull('origin', 'develop');
+  logger.info('Pulling branches and tags...');
+  if (!dryRun) {
+    await dependencies.git.fetch(['--tags']);
+    await dependencies.git.pull('origin', 'master');
+    await dependencies.git.pull('origin', 'develop');
+  } else {
+    logger.info('Would normally pull branches and tags, but skipping due to dry run');
+  }
 
   // generate changelog of develop:latest vs master:latest
   // show changelog to user
   // const changelog = await createChangelog();
-  // console.log(renderChangelog(changelog));
+  // logger.info(renderChangelog(changelog));
 
   // get version from package.json
   const packageFile = await dependencies.readFile(packageJSONFilename);
   const packageJSON = JSON.parse(packageFile.toString());
-  var version = packageJSON.version;
+  let version = packageJSON.version;
   logger.info(`Current package.json version is ${version}`);
 
   // ask user if this is major/minor/patch
-  var answer: PatchTypeAnswer;
+  let answer: PatchTypeAnswer;
   answer = await inquirer.prompt({
     type: 'list',
     name: 'release_type',
     message: 'What type of release is this?',
     choices: ['Major', 'Minor', 'Patch'],
     default: 'Patch',
-    filter: function(val) {
+    filter: (val) => {
       return val.toLowerCase();
-    }
+    },
   });
 
   // bump version according to what user said earlier
@@ -72,7 +85,7 @@ export default async function beginRelease(args: string[], options: Options) {
   // make a branch off develop called release/x.y.z
   const newBranchName = `release/${version}`;
   if (!dryRun) {
-    await git.checkoutBranch(newBranchName, 'develop');
+    await dependencies.git.checkoutBranch(newBranchName, 'develop');
   } else {
     logger.info('Would normally checkout branch, but skipping due to dry run.');
   }
@@ -80,21 +93,27 @@ export default async function beginRelease(args: string[], options: Options) {
   // commit new version to package.json
   packageJSON.version = version;
   if (!dryRun) {
-    await dependencies.writeFile(packageJSONFilename, JSON.stringify(packageJSON, null, 2));
-    await git.add(packageJSONFilename);
-    await git.commit(`Bump package.json to ${version}`);
+    const indentSpacing = 2;
+    await dependencies.writeFile(packageJSONFilename, JSON.stringify(packageJSON, null, indentSpacing));
+    await dependencies.git.add(packageJSONFilename);
+    await dependencies.git.commit(`Bump version to ${version}`);
   } else {
-    logger.info('Would normally update package.json and commit it, but skipping due to dry run.')
+    logger.info('Would normally update package.json and commit it, but skipping due to dry run.');
   }
 
   // push to origin
   if (!dryRun) {
-    await git.push('origin', newBranchName);
+    await dependencies.git.push('origin', newBranchName);
   } else {
     logger.info('Would normally push release branch to origin, but skipping due to dry run.');
   }
 
   // generate PR using github API and put in changelog (optional)
+  if (!dryRun) {
+    await dependencies.exec(`hub pull-request -b master -m "<changelog goes here>"`);
+  } else {
+    logger.info('Would normally create pull request against master, but skipping due to dry run.');
+  }
 }
 
 /** Returns the help message for the command */
@@ -103,7 +122,7 @@ export function helpMessage() {
     'Description:',
     '  Begins the release process for this module.\n',
     'Usage:',
-    `  ts-node createRelease.ts`,
+    '  ts-node createRelease.ts',
   ].join('\n');
 }
 
