cf8cdb26d6dc63117a72d3839514d8fd63cf2f52
Myron Langlais <myron.langlais@diffblue.com>
2019-04-30 14:06:25 +0100

    Add parameter handling to api string generator

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,10 +2,24 @@
 
 import urljoin from 'url-join';
 
+export const generateApiUrl = (params: string[]) => {
+  if (params.length <= 1) {
+    throw new Error('At least 2 parameters are required to generate a valid Cover URL');
+  }
+
+  for (const param of params) {
+    if (param === '') {
+      throw new Error('Route parameter cannot be an empty string');
+    }
+  }
+
+  return urljoin(params);
+};
+
 export const routes = {
-  version: (api: string) => urljoin([api, 'version']),
-  start: (api: string) => urljoin([api, 'analysis']),
-  result: (api: string, identifier: string) => urljoin([api, 'analysis', identifier]),
-  status: (api: string, identifier: string) => urljoin([api, 'analysis', identifier, 'status']),
-  cancel: (api: string, identifier: string) => urljoin([api, 'analysis', identifier, 'cancel']),
+  version: (api: string) => generateApiUrl([api, 'version']),
+  start: (api: string) => generateApiUrl([api, 'analysis']),
+  result: (api: string, identifier: string) => generateApiUrl([api, 'analysis', identifier]),
+  status: (api: string, identifier: string) => generateApiUrl([api, 'analysis', identifier, 'status']),
+  cancel: (api: string, identifier: string) => generateApiUrl([api, 'analysis', identifier, 'cancel']),
 };
