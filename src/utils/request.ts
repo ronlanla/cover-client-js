30ee940d950f957a8f8f5789f9291cb434c52d30
Myron Langlais <myron.langlais@diffblue.com>
2019-05-03 13:57:23 +0100

    Simplify request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,27 +1,32 @@
 // Copyright 2018-2019 Diffblue Limited. All Rights Reserved.
 
-import axios from 'axios';
-import urljoin from 'url-join';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import FormData from 'form-data';
+
+/** Platform Lite API error class */
+export class ApiError extends Error {
+  public constructor(message: string, code: string) {
+    super(message);
+    this.code = code;
+  }
+  public code: string;
+}
+
+/** Convert an axios error into the standard Platform Lite API format */
+export const convertError = (error: AxiosError) => {
+  const data = error.response && error.response.data;
+  if (data && data.message && data.code) {
+    throw new ApiError(data.message, data.code);
+  }
+  throw new ApiError('An unknown error occurred', 'unknown-error');
+};
 
 const request = {
-  get: function(path: string, parameters?: any, headers?: { [key: string]: string }) {
-    return axios.get(
-      urljoin(process.env.API_HOST || '/', path),
-      Object.assign({ withCredentials: Boolean(process.env.ENABLE_AUTH), params: parameters || {} }, headers ? { headers: headers } : undefined),
-    );
+  get: async (path: string, config?: AxiosRequestConfig) => {
+    return axios.get(path, config).then((response: AxiosResponse) => response.data).catch(convertError);
   },
-  post: function(path: string, data: any, headers?: { [key: string]: string }) {
-    return axios.post(
-      urljoin(process.env.API_HOST || '/', path),
-      data,
-      Object.assign({ withCredentials: Boolean(process.env.ENABLE_AUTH) }, headers ? { headers: headers } : undefined),
-    );
-  },
-  delete: function(path: string, headers?: { [key: string]: string }) {
-    return axios.delete(
-      urljoin(process.env.API_HOST || '/', path),
-      Object.assign({ withCredentials: Boolean(process.env.ENABLE_AUTH) }, headers ? { headers: headers } : undefined),
-    );
+  post: async (path: string, data?: FormData, config?: AxiosRequestConfig) => {
+    return axios.post(path, data, config).then((response: AxiosResponse) => response.data).catch(convertError);
   },
 };
 
