f9bfaa51c412adbbeddd97c4317eb72288a00687
James Hannett <james.hannett@diffblue.com>
2019-05-03 10:49:48 +0100

    Better error handling, review feedback and removed dry run option

--- src/scripts/createRelease.ts
+++ src/scripts/createRelease.ts
@@ -4,7 +4,7 @@ import { exec } from 'child_process';
 import { readFile, writeFile } from 'fs';
 import * as inquirer from 'inquirer';
 import * as semver from 'semver';
-import * as simplegit from 'simple-git/promise';
+import * as simpleGit from 'simple-git/promise';
 import { promisify } from 'util';
 import argvParser, { Options } from '../utils/argvParser';
 import { createChangelog, renderChangelogVersion } from '../utils/changelog';
@@ -14,113 +14,105 @@ import logger from '../utils/log';
 const dependencies = {
   writeFile: promisify(writeFile),
   readFile: promisify(readFile),
-  git: simplegit(),
+  simpleGit: simpleGit(),
   exec: promisify(exec),
 };
 
-const packageJSONFilename = 'package.json';
+const packageJsonFilename = 'package.json';
 
 /** Assurance that 'release_type' will be a member of what inquirer.js will return */
 type PatchTypeAnswer = {
-  release_type: semver.ReleaseType;
+  releaseType: semver.ReleaseType;
 };
 
 /**
  * Begins the release process
  *
  * Ensures the Git repo is up to date, bumps the version and creates a PR.
- *
- * @param {string[]} args - Array of command line arguments
- * @param {Options} options - Collection of arguments
  */
-export default async function beginRelease(args: string[], options: Options) {
+export default async function createRelease(args: string[], options: Options) {
   if (options.help) {
     logger.log(helpMessage());
-    return;
+    return undefined;
   }
+  const forceRelease = Boolean(options.force);
 
-  let dryRun = false;
-  if (options.dry) {
-    dryRun = true;
-  }
-
-  // make sure develop and master are up to date
-  // also get all tags
+  // Make sure develop and master are up to date & get all tags
   logger.info('Pulling branches and tags...');
-  if (!dryRun) {
-    await dependencies.git.fetch(['--tags']);
-    await dependencies.git.pull('origin', 'master');
-    await dependencies.git.pull('origin', 'develop');
-  } else {
-    logger.info('Would normally pull branches and tags, but skipping due to dry run');
+  await dependencies.simpleGit.fetch(['--tags']);
+  await dependencies.simpleGit.pull('origin', 'master');
+  await dependencies.simpleGit.pull('origin', 'develop');
+
+  logger.info('Checking out \'develop\` and making sure branch is clean');
+  await dependencies.simpleGit.checkoutLocalBranch('develop');
+  const developStatus = await dependencies.simpleGit.status();
+  if (!developStatus.isClean()) {
+    logger.error('Branch \'develop\' is not clean. Please stash or commit your changes before attempting release');
+    return 1;
   }
 
-  // generate changelog of develop:latest vs master:latest
-  // show changelog to user
+  // Generate changelog of develop:latest vs master:latest
   const changelog = await createChangelog();
-  let changes = '';
-  for (const version of changelog) {
-    if (version.version === 'Unreleased') {
-      changes = renderChangelogVersion(version).trim();
-    }
+  const changes = (
+    changelog
+    .filter((version) => version.version === 'Unreleased')
+    .map((version) => renderChangelogVersion(version).trim())
+  );
+
+  if (changes.length === 0 && !forceRelease) {
+    logger.error('No changes detected in changelog. Is there anything to release?');
+    return 1;
   }
 
-  // get version from package.json
-  const packageFile = await dependencies.readFile(packageJSONFilename);
-  const packageJSON = JSON.parse(packageFile.toString());
-  let version = packageJSON.version;
-  logger.info(`Current package.json version is ${version}`);
+  // Get version from package.json
+  const packageFile = await dependencies.readFile(packageJsonFilename)
+  .catch((err) => {
+    logger.error(`Unable to read package.json: ${err}`);
+    return 1;
+  });
+  const packageJson = JSON.parse(packageFile.toString());
+  const originalVersion = packageJson.version;
+  logger.info(`Current package.json version is ${originalVersion}`);
 
-  // ask user if this is major/minor/patch
-  let answer: PatchTypeAnswer;
-  answer = await inquirer.prompt({
+  // Ask user if this is major/minor/patch
+  const answer: PatchTypeAnswer = await inquirer.prompt({
     type: 'list',
-    name: 'release_type',
+    name: 'releaseType',
     message: 'What type of release is this?',
     choices: ['Major', 'Minor', 'Patch'],
     default: 'Patch',
-    filter: (val) => {
-      return val.toLowerCase();
-    },
+    filter: (val) => val.toLowerCase(),
   });
 
-  // bump version according to what user said earlier
-  version = semver.inc(version, answer.release_type);
-  logger.info(`New package.json version is ${version}`);
+  // Bump version according to what user said earlier
+  const newVersion = semver.inc(originalVersion, answer.releaseType);
+  logger.info(`New package.json version is ${newVersion}`);
 
-  // make a branch off develop called release/x.y.z
-  const newBranchName = `release/${version}`;
-  if (!dryRun) {
-    await dependencies.git.checkoutBranch(newBranchName, 'develop');
-  } else {
-    logger.info('Would normally checkout branch, but skipping due to dry run.');
-  }
+  // Make a branch off develop called release/x.y.z
+  const newBranchName = `release/${newVersion}`;
+  await dependencies.simpleGit.checkoutBranch(newBranchName, 'develop');
 
-  // commit new version to package.json
-  packageJSON.version = version;
-  if (!dryRun) {
-    const indentSpacing = 2;
-    await dependencies.writeFile(packageJSONFilename, JSON.stringify(packageJSON, null, indentSpacing));
-    await dependencies.git.add(packageJSONFilename);
-    await dependencies.git.commit(`Bump version to ${version}`);
-  } else {
-    logger.info('Would normally update package.json and commit it, but skipping due to dry run.');
-  }
+  // Commit new version to package.json
+  packageJson.version = newVersion;
+  const indentSpacing = 2;
+  await dependencies.writeFile(packageJsonFilename, JSON.stringify(packageJson, null, indentSpacing))
+  .catch((err) => {
+    logger.error(`Unable to write package.json: ${err}`);
+    return 1;
+  });
+  await dependencies.simpleGit.add(packageJsonFilename);
+  await dependencies.simpleGit.commit(`Bump version to ${newVersion}`);
 
-  // push to origin
-  if (!dryRun) {
-    await dependencies.git.push('origin', newBranchName);
-  } else {
-    logger.info('Would normally push release branch to origin, but skipping due to dry run.');
-  }
+  // Push to origin
+  await dependencies.simpleGit.push('origin', newBranchName);
 
-  // generate PR using github API and put in changelog (optional)
-  if (!dryRun) {
-    await dependencies.exec(`hub pull-request -b master -m "${changes}"`);
-  } else {
-    logger.info('Would normally create pull request against master, but skipping due to dry run.');
-    logger.info(`hub pull-request -b master -m "${changes}"`);
-  }
+  // Generate PR using github API and put in changelog (optional)
+  await dependencies.exec(
+    `hub pull-request -b master -m "Release ${newVersion}
+
+${changes}"`);
+
+  return 0;
 }
 
 /** Returns the help message for the command */
@@ -128,15 +120,15 @@ export function helpMessage() {
   return [
     'Description:',
     '  Begins the release process for this module.',
-    '  Use --dry argument to avoid changing any files.\n',
+    '  Use --force argument to do a release even if the changelog is empty.\n',
     'Usage:',
-    '  ts-node createRelease.ts [--dry]',
+    '  ts-node createRelease.ts [--force]',
   ].join('\n');
 }
 
 if (require.main === module) {
   const { args, options } = argvParser(process.argv);
-  beginRelease(args, options).catch((error) => {
+  createRelease(args, options).catch((error) => {
     logger.error(`${helpMessage()}\n${error.toString()}`);
     process.exit(1);
   });
