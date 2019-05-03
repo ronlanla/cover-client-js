90ab56b08ce16d8caa52285a58a133c817550ef6
James Hannett <james.hannett@diffblue.com>
2019-05-03 10:50:13 +0100

    Update copyright and add yarn command for beginning release

--- src/types/glob-gitignore/index.d.ts
+++ src/types/glob-gitignore/index.d.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2019 Diffblue Limited. All Rights Reserved.
+// Copyright 2019 Diffblue Limited. All Rights Reserved.
 
 declare module 'glob-gitignore' {
   export function glob(patterns: string | string[], options: { [key: string]: any }): Promise<string[]>;
