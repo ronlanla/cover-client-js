11de307d1cd6f7e09fa6952d663799f13fc6d5ec
James Hannett <james.hannett@diffblue.com>
2019-05-08 14:53:05 +0100

    Tests for createRelease

--- tests/unit/scripts/createRelease.ts
+++ tests/unit/scripts/createRelease.ts
@@ -1,17 +1,21 @@
 // Copyright 2019 Diffblue Limited. All Rights Reserved.
 
-import assert from '../../../src/utils/assertExtra';
-
-import createRelease from '../../../src/scripts/createRelease';
+import createRelease, { dependencies } from '../../../src/scripts/createRelease';
 import { Options } from '../../../src/utils/argvParser';
+import assert from '../../../src/utils/assertExtra';
+import sinonTestFactory from '../../../src/utils/sinonTest';
+import sinon = require('sinon');
+
+const sinonTest = sinonTestFactory();
 
 describe('scripts/createRelease', () => {
   describe('createRelease', () => {
     const options:Options = {};
     options.dryRun = true;
 
-    it('Returns 0', () => {
-      assert.deepStrictEqual(createRelease([], options), 0);
+    it('Returns "Release process began successfully"', () => {
+      const writeFile = sinon.stub(dependencies.inquirer, 'prompt');
+      assert.deepStrictEqual(createRelease(false), 'Release process began successfully');
     });
   });
 });
