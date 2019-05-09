9f3845800f5d8a3face76375cbd606f65bc704be
Peter West <peterjwest3@gmail.com>
2019-05-09 15:49:24 +0100

    Add script to create a post-release pull request back into develop

---
+++ src/scripts/createPostReleasePullRequest.ts
@@ -0,0 +1,39 @@
+// Copyright 2019 Diffblue Limited. All Rights Reserved.
+
+
+import commandLineRunner, { ExpectedError } from '../utils/commandLineRunner';
+import createPullRequest from '../utils/createPullRequest';
+import getPackageJson from '../utils/getPackageJson';
+
+export const dependencies = {
+  getPackageJson: getPackageJson,
+};
+
+/** Creates a pull request on Github from master back to develop */
+export async function createPostReleasePullRequest(args: string[]) {
+  const token = args[0];
+  if (!token) {
+    throw new ExpectedError('Please provide a token to authenticate with Github');
+  }
+
+  const packageJson = await dependencies.getPackageJson();
+  const repositoryMatch = packageJson.repository.url.match(/^git@github\.com:(.+)\.git$/);
+
+  if (!repositoryMatch) {
+    throw new ExpectedError('Could not extract repository name from package.json repository.url');
+  }
+
+  await createPullRequest(
+    repositoryMatch[1],
+    token,
+    `Test: Merge ${packageJson.version} back into develop`,
+    packageJson.version,
+    'develop',
+  );
+  return `Created pull request to merge version ${packageJson.version} back into develop`;
+}
+
+/* istanbul ignore next */
+if (require.main === module) {
+  commandLineRunner('Creates and pushes a tag for the current version', '<token>', createPostReleasePullRequest);
+}
---
+++ src/utils/createPullRequest.ts
@@ -0,0 +1,32 @@
+// Copyright 2019 Diffblue Limited. All Rights Reserved.
+
+import axios from 'axios';
+
+import { ExpectedError } from './commandLineRunner';
+import spawn from './spawnProcess';
+
+export const dependencies = {
+  spawn: spawn,
+};
+
+/** Creates a pull request on Github */
+export default async function createPullRequest(
+  repo: string, token: string, title: string, head: string, base: string,
+) {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `token ${token}`,
+  };
+  await axios.post(`https://api.github.com/repos/${repo}/pulls`, {
+    title: title,
+    head: head,
+    base: base,
+  }, { headers: headers })
+  .catch((error) => {
+    if (error.response.statusText === 'Unauthorized') {
+      throw new ExpectedError('Invalid authorization token');
+    }
+    const errorData = error.response.data;
+    throw new Error(`Github API error: ${JSON.stringify(errorData, null, '  ')}`);
+  });
+}
