0525334ca8f28b5b081979432a13419c0134ab72
Myron Langlais <myron.langlais@diffblue.com>
2019-05-29 17:42:50 +0100

    Add start analysis mock response to server

--- api-mock/src/routers/analysis.ts
+++ api-mock/src/routers/analysis.ts
@@ -1,13 +1,49 @@
 // Copyright 2019 Diffblue Limited. All Rights Reserved.
 
 import { Router } from 'express';
+import * as StatusCode from 'http-status-codes';
+
+import { ApiError } from '../../../src/errors';
+import { AnalysisSettings } from '../../../src/types/types';
+import { RequestConfig } from '../../types';
+import { getRequestFile, uploadFiles } from '../handlers/file';
 
 const router = Router();
 
 // Start analysis route
-router.post('', (req, res, next) => {
-  return res.send('start analysis');
-});
+router.post(
+  '',
+  uploadFiles(['build', 'dependenciesBuild', 'baseBuild', 'settings']),
+  (req: RequestConfig, res, next) => {
+    if (!getRequestFile(req, 'build')) {
+      return next(new ApiError(
+        'The required `build` JAR file was not supplied',
+        'buildMissing',
+        StatusCode.BAD_REQUEST,
+      ));
+    }
+
+    const state = req.app.locals.state;
+    let settings: AnalysisSettings;
+
+    if (!state.analysisId) {
+      return next(new ApiError(
+        'Current state does not contain analysis id, please set a valid state',
+        'stateInvalid',
+        StatusCode.BAD_REQUEST,
+      ));
+    }
+
+    try {
+      settings = JSON.parse(req.body.settings);
+    } catch (error) {
+      return next(new ApiError(`Invalid analysis settings: ${error}`, 'settingsInvalid', StatusCode.BAD_REQUEST));
+    }
+
+    res.send({ id: state.analysisId, phases: settings.phases || state.phases });
+    next();
+  },
+);
 
 // Analysis results route
 router.get('/:analysis', (req, res, next) => {
